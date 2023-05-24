de3ba9c6e7a4e30903070d684a7fbb84d7c6f449
Daniel Buxton Sierras <kylorencodes@gmail.com>
2023-05-24 13:59:06 +0200

    Añadir tamaño de clusters, añadir entradas de fat y cambiar nombre de método para mostrar opciones del menú

--- SistemaDeFicherosFAT/src/com/fat/SistemaDeFicheros.java
+++ SistemaDeFicherosFAT/src/com/fat/SistemaDeFicheros.java
@@ -6,12 +6,30 @@ public class SistemaDeFicheros {
 	
 	private static final int ES_MAC = 0;
 	private static final int ES_WINDOWS = 1;
-	int numeroClusters;
 	
-	ArrayList<Cluster> clustersSistemaDeFicheros = new ArrayList<Cluster>();
+	int tipoDeSistemaDeFicheros;
+	int numeroClusters;
+	int sizeClusters;
+	
+	ArrayList<Cluster> clustersSistemaDeFicheros;
+	ArrayList<EntradaFAT> entradasSistemaDeFicheros;
 
 	//LANZAMIENTO SISTEMA FICHEROS FAT
 	public static void main(String[] args) {
+		
+	}
+	
+	public SistemaDeFicheros(int tipoDeSistemaDeFicheros, int numeroClusters, int sizeClusters) {
+		this.tipoDeSistemaDeFicheros = tipoDeSistemaDeFicheros;
+		this.numeroClusters = numeroClusters;
+		this.sizeClusters = sizeClusters;
+		
+		this.clustersSistemaDeFicheros = new ArrayList<Cluster>();
+		this.entradasSistemaDeFicheros = new ArrayList<EntradaFAT>();
+		
+		for(int cluster=0; cluster<numeroClusters; cluster++) {
+			clustersSistemaDeFicheros.add(new Cluster);
+		}
 		
 	}
 	
